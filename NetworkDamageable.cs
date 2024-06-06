ed1d32250aa86c6d9e5ea5b243f404fc48032c5a
Serinoxxx <lancemclachlan@gmail.com>
2024-06-07 09:38:32 +1200

    added critical syncing

--- NetworkDamageable.cs
+++ NetworkDamageable.cs
@@ -10,14 +10,17 @@ namespace MalbersAnimations.NetCode
     public class NetworkDamageable : NetworkBehaviour
     {
         MDamageable _damageable;
+        ServerAuthAnimal _serverAuthAnimal;
 
         private void Start()
         {
             _damageable = GetComponent<MDamageable>();
+            _serverAuthAnimal = GetComponent<ServerAuthAnimal>();
         }
 
         public void SendDamageToClients(float amount)
         {
+            
             Debug.Log($"{gameObject.name} received {amount} damage");
 
             //Only host should handle the events
@@ -25,14 +28,33 @@ namespace MalbersAnimations.NetCode
             { return; }
 
             Debug.Log($"Sending {amount} damage to clients");
-            ReceiveDamageRpc(amount);
+            ReceiveDamageRpc(amount, _damageable.LastDamage.WasCritical, _damageable.HitDirection);
         }
 
         [Rpc(SendTo.NotMe)]
-        void ReceiveDamageRpc(float amount)
+        void ReceiveDamageRpc(float amount, bool wasCritical, Vector3 direction)
         {
             Debug.Log($"{gameObject.name} just took {amount} damage!");
-            _damageable.ReceiveDamage(Vector3.forward, gameObject, _damageable.stats.stats[0].ID, amount, false, true, null, false);
+            _damageable.ReceiveDamage(direction, gameObject, _damageable.stats.stats[0].ID, amount, wasCritical, true, null, false);
         }
+
+        // public void SendCriticalDamageToClients()
+        // {
+        //     Debug.Log($"{gameObject.name} received critical damage");
+
+        //     //Only host should handle the events
+        //     if (!IsServer)
+        //     { return; }
+
+        //     Debug.Log($"Sending critical damage to clients");
+        //     ReceiveCriticalDamageRpc();
+        // }
+
+        // [Rpc(SendTo.NotMe)]
+        // void ReceiveCriticalDamageRpc()
+        // {
+        //     Debug.Log($"{gameObject.name} just took critical damage!");
+        //     _damageable.criticalReaction.TryReact(_serverAuthAnimal);
+        // }
     }
 }
